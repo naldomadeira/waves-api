66770a7baf0ce2048482f9a5fc05c814ef767c9a
Francinaldo Madeira <francinaldo.madeira@multilaser.com.br>
2022-01-29 01:27:05 -0300

    feat (waves.api): added mongoDB, controller, functional tests and model for beaches

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
-import { ForecastController } from './controllers/forecast';
+import { ForecastController } from '@src/controllers/forecast';
+import { BeachesController } from '@src/controllers/beaches';
 import { Application } from 'express';
-
+import * as database from '@src/database';
 export class SetupServer extends Server {
     constructor(private port = 3000) {
         super();
     }
 
-    public init(): void {
+    public async init(): Promise<void> {
         this.setupExpress();
         this.setupControllers();
+        await this.databaseSetup();
     }
 
     private setupExpress(): void {
@@ -20,10 +22,19 @@ export class SetupServer extends Server {
 
     private setupControllers(): void {
         const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
+        const beachesController = new BeachesController();
+        this.addControllers([forecastController, beachesController]);
     }
 
     public getApp(): Application {
         return this.app;
+    }
+
+    private async databaseSetup(): Promise<void> {
+        await database.connect();
+    }
+
+    public async close(): Promise<void> {
+        await database.close();
     }
 }
